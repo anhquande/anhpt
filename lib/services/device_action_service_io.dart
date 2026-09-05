4ab6b62eaf1a42761f2a73a37addd6c8e9cc9448
Anhquan <anhquande@users.noreply.github.com>
2026-09-05 16:31:28 +0200

    fix: support screen blackout on Android

--- lib/services/device_action_service_io.dart
+++ lib/services/device_action_service_io.dart
@@ -1,8 +1,18 @@
 import 'dart:io';
 
+import 'package:screen_brightness/screen_brightness.dart';
 import 'package:win32/win32.dart';
 
 Future<bool> turnOffDisplayOnPlatform() async {
+  if (Platform.isAndroid) {
+    try {
+      await ScreenBrightness.instance.setApplicationScreenBrightness(0);
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
+
   if (!Platform.isWindows) return false;
   final targetWindow = GetForegroundWindow();
   if (targetWindow == 0) return false;
@@ -12,4 +22,16 @@ Future<bool> turnOffDisplayOnPlatform() async {
   // machine power action instead of monitor power-off.
   SendMessage(targetWindow, WM_SYSCOMMAND, SC_MONITORPOWER, 2);
   return true;
+}
+
+Future<bool> restoreDisplayOnPlatform() async {
+  if (Platform.isAndroid) {
+    try {
+      await ScreenBrightness.instance.resetApplicationScreenBrightness();
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
+  return Platform.isWindows;
 }
