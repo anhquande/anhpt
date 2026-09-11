84a5f298cbd743c02b8cfd8ea5ae80e2c92c98ac
Anhquan <anhquande@users.noreply.github.com>
2026-09-11 12:28:46 +0200

    refactor: export pose feature API

--- lib/core/pose/pose.dart
+++ lib/core/pose/pose.dart
@@ -1,6 +1,7 @@
 /// Engine-agnostic pose domain API for AnhPT.
 export 'body_pose.dart';
 export 'pose_estimator.dart';
+export 'pose_features.dart';
 export 'pose_frame.dart';
 export 'pose_geometry.dart';
 export 'pose_pipeline.dart';
